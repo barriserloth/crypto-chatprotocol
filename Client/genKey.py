378f1a0ed89bc158ba2ccfec194889f41e62b88f
barriserloth <iserlothb@carleton.edu>
2016-11-26 12:22:36 +0100

    Keys for all 4 currently registered users

--- Client/genKey.py
+++ Client/genKey.py
@@ -1,16 +1,17 @@
 from Crypto.PublicKey import RSA
+from Crypto import Random
 
-key = RSA.generate(2048)
+rand_gen = Random.new().read
+key = RSA.generate(2048, rand_gen)
 
 pub_key = key.publickey().exportKey("PEM")
 priv_key = key.exportKey("PEM")
 
 #writing public key to file
-pubKey = open('pubkey.pem', 'w')
+pubKey = open('addisonPubKey.pem', 'w')
 pubKey.write(pub_key)
 pubKey.close()
 
-privKey = open('privkey.pem', 'w')
+privKey = open('addisonPrivKey.pem', 'w')
 privKey.write(priv_key)
 privKey.close()
-
